35d2c574c9801340015d415f068c02881298983d
Mohamed Khalefa <m.elbasiouny@gmail.com>
2013-12-05 02:22:32 +0200

    BNL

--- src/skyhadoop/BNL.java
+++ src/skyhadoop/BNL.java
@@ -1,27 +1,15 @@
 package skyhadoop;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import org.apache.hadoop.mapred.*;
-import java.net.InetSocketAddress;
-import java.net.SocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.channels.DatagramChannel;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Vector;
 
-import org.apache.hadoop.fs.FileSystem;
+import javax.sound.midi.Sequence;
+
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.FileOutputFormat;
@@ -32,6 +20,9 @@ import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.SequenceFileInputFilter;
+import org.apache.hadoop.mapred.SequenceFileInputFormat;
+import org.apache.hadoop.mapred.SequenceFileOutputFormat;
 import org.apache.hadoop.mapred.TextInputFormat;
 import org.apache.hadoop.mapred.TextOutputFormat;
 
@@ -52,62 +43,116 @@ public class BNL {
 	}
 
 	public static class IdentityMapper extends MapReduceBase implements
-			Mapper<LongWritable, Text, LongWritable, Text> {
+			Mapper<LongWritable, PointWritable, LongWritable, PointWritable> {
 
 		final LongWritable one = new LongWritable(1);
 
-		public void map(LongWritable key, Text value,
-				OutputCollector<LongWritable, Text> output, Reporter reporter)
-				throws IOException {
-			String[] val = value.toString().trim().split("\t");
-			output.collect(one, new Text(value));
+		public void map(LongWritable key, PointWritable value,
+				OutputCollector<LongWritable, PointWritable> output,
+				Reporter reporter) throws IOException {
+
+			output.collect(one, value);
 		}
 	}
 
-	public static class SkyReducer extends MapReduceBase implements
+	// Point --> text
+	public static class SkyReducer_PT extends MapReduceBase implements
 			Reducer<LongWritable, PointWritable, LongWritable, Text> {
 
 		public void reduce(LongWritable n, Iterator<PointWritable> values,
-				OutputCollector<LongWritable, Text> output,
-				Reporter reporter) throws IOException {
+				OutputCollector<LongWritable, Text> output, Reporter reporter)
+				throws IOException {
 
 			Vector<Point> points = new Vector<Point>();
 			while (values.hasNext()) {
-				Point p= new Point(values.next());
+				Point p = new Point(values.next());
 				points.add(p);
 			}
 			Skyline skyline = new Skyline(points);
 			skyline.Compute();
 
-			for (int i=0;i<skyline.skylines .size();i++) {
-				PointWritable s=new PointWritable(points.get(i));
-				output.collect(new LongWritable(s.id),  new Text(s.toString()) );
+			for (int i = 0; i < skyline.skylines.size(); i++) {
+				PointWritable s = new PointWritable(points.get(i));
+				output.collect(new LongWritable(s.id), new Text(s.toString()));
 			}
 		}
 	}
 
-	static JobConf conf = new JobConf(Skyline.class);
+	public static class SkyReducer_PP extends MapReduceBase implements
+			Reducer<LongWritable, PointWritable, LongWritable, PointWritable> {
 
-	public static void main(String[] args) throws Exception {
-		// long start = System.currentTimeMillis();
+		public void reduce(LongWritable n, Iterator<PointWritable> values,
+				OutputCollector<LongWritable, PointWritable> output,
+				Reporter reporter) throws IOException {
 
-		conf.setJobName("BNL");
+			Vector<Point> points = new Vector<Point>();
+			while (values.hasNext()) {
+				Point p = new Point(values.next());
+				points.add(p);
+			}
+			Skyline skyline = new Skyline(points);
+			skyline.Compute();
+
+			for (int i = 0; i < skyline.skylines.size(); i++) {
+				PointWritable s = new PointWritable(points.get(i));
+				output.collect(new LongWritable(s.id), s);
+			}
+		}
+	}
+
+	static JobConf conf = new JobConf(BNL.class);
+	static JobConf gconf = new JobConf(BNL.class);
+
+	static public void Divide(JobConf conf, String[] args) throws Exception {
+
+		conf.setJobName("BNL-divide");
 
 		conf.setOutputKeyClass(LongWritable.class);
 		conf.setOutputValueClass(PointWritable.class);
 
 		conf.setMapperClass(MapDivision.class);
-		// conf.setCombinerClass(GlobalReducer.class);
-		conf.setReducerClass(SkyReducer.class);
-		conf.setNumReduceTasks(2);
-		conf.setNumMapTasks(2);
+		conf.setCombinerClass(SkyReducer_PP.class);
+		conf.setReducerClass(SkyReducer_PP.class);
+		conf.setNumReduceTasks(1);
+		conf.setNumMapTasks(1);
 
 		conf.setInputFormat(TextInputFormat.class);
-		conf.setOutputFormat(TextOutputFormat.class);
+		conf.setOutputFormat(SequenceFileOutputFormat.class);
 
 		FileInputFormat.setInputPaths(conf, new Path(args[0]));
-		FileOutputFormat.setOutputPath(conf, new Path(args[1] + "/res"));
+		FileOutputFormat.setOutputPath(conf, new Path(args[1] + "/tmp"));
 		JobClient.runJob(conf);
+	}
+
+	static public void Gather(JobConf conf, String[] args) throws Exception {
+
+		conf.setJobName("BNL-Gather");
+
+		conf.setOutputKeyClass(LongWritable.class);
+		conf.setOutputValueClass(PointWritable.class);
+
+		conf.setMapperClass(IdentityMapper.class);
+		conf.setCombinerClass(SkyReducer_PP.class);
+		conf.setReducerClass(SkyReducer_PT.class);
+		conf.setNumReduceTasks(1);
+		conf.setNumMapTasks(1);
+
+		conf.setInputFormat(SequenceFileInputFormat.class);
+		conf.setOutputFormat(TextOutputFormat.class);
+
+		FileInputFormat.setInputPaths(conf, new Path(args[1] + "/tmp"));
+		FileOutputFormat.setOutputPath(conf, new Path(args[1] + "/r"));
+		JobClient.runJob(conf);
+	}
+
+	public static void main(String[] args) throws Exception {
+		long start = System.currentTimeMillis();
+		Divide(conf, args);
+		Gather(gconf, args);
+		long end = System.currentTimeMillis();
+		FileWriter writer2 = new FileWriter("jobTime", true);
+		writer2.write(end - start + "\n");
+		writer2.close();
 
 	}
 }
--- src/skyhadoop/Partioner.java
+++ src/skyhadoop/Partioner.java
@@ -1,7 +1,17 @@
 package skyhadoop;
 
 public class Partioner {
+	static final int sizeX=1000;
+	static final int sizeY=1000;
+	public static int gridX=100;
+	public static int gridY=100;
+	
+	
 	public static int gridpartioner(Point p) {
-		return 0;
+        double x=p.d[0]/gridX;
+        double y=p.d[1]/gridY;
+        
+        return (int)(x*sizeX/gridX + y);
+		
 	}
 }
