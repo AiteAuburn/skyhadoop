16c0db1f0bac8b1857972d7c7c924142f923a6b7
hadoopuser <waleed.lutfi90@gmail.com>
2014-01-05 23:46:23 +0200

    Reservoir Sampling

---
+++ src/skyhadoop/Sampler.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package skyhadoop;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ *
+ * @author hadoopuser
+ */
+public class Sampler {
+
+    public static List reservoirSampling(List<PointWritable> inp, int k)  {
+        List<PointWritable> reservoirList = new ArrayList<PointWritable>(k);   //reservoirList is where our selected lines stored
+        int count = 0;                           // we will use this counter to count the current line numebr while iterating
+        Random ra = new Random();
+
+        int randomNumber;
+        for (int i = 0; i < inp.size(); i++) {
+            {
+                count++;                                         // increase the line number
+                if (count <= 10) {
+                    reservoirList.add(inp.get(i));
+                } else {
+                    randomNumber = (int) ra.nextInt(count);
+                    if (randomNumber < k) {
+                        reservoirList.set(randomNumber, inp.get(i));
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
